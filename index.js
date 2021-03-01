0ef1f4117a8b5b68c1ce42ebd4c7e9a08651c80a
Kapone <69044934+Kapone-dev@users.noreply.github.com>
2021-03-01 18:06:30 -0300

    Codigo sincronico

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const hola = require("./requests.js");
 const funcion = new hola.requestApi;
 
-async function main(server) {
-    const funcionxd = await funcion.server(server)
-    console.log(funcionxd)
-};
-
-main("mineaqua.us")
+funcion.server(server).then(req => {
+console.log(req)
+})
